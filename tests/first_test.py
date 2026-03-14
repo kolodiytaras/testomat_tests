3794d1108c3d179e3965ad6b958691f431951523
Taras Kolodiy <kolodiytaras@gmail.com>
2026-03-14 12:18:46 +0200

    Add initial setup for Playwright tests

---
+++ tests/first_test.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import Page, expect
+
+
+def test_login_with_invalid_creds(page: Page):
+    page.goto("https://testomat.io/")
+
+    expect(page.locator("[href*='sign_in'].login-item")).to_be_visible()
+
+    page.get_by_text("Log in", exact=True).click()
+
+    page.locator("#content-desktop #user_email").fill("taras.testing.2026@gmail.com")
+    page.locator("#content-desktop #user_password").fill("12345")
+    page.get_by_role("button", name="Sign in").click()
+
+    expect(page.locator("#content-desktop").get_by_text("Invalid Email or password.")).to_be_visible()
+    expect(page.locator("#content-desktop .common-flash-info")).to_have_text("Invalid Email or password.")
+
